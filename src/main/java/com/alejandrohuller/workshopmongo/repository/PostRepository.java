16e516f104234c56bf0ef6174657ea5856f727fa
Alejandro <ale_huller@hotmail.com>
2024-05-21 15:53:07 -0300

    Consulta simples com query methods

--- src/main/java/com/alejandrohuller/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/alejandrohuller/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.alejandrohuller.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,5 @@ import com.alejandrohuller.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
 	
+	List<Post> findByTitleContainingIgnoreCase(String text);
 }
